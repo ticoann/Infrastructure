60f0f528a9f55963a12ac63b6307bedc66b33b38
diego <diego@4525493e-7705-40b1-a816-d608a930855b>
2010-11-02 15:31:15 +0000

    cleans old CMSDIST tags
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@10807 4525493e-7705-40b1-a816-d608a930855b

---
+++ BuilderAgent/tag-cleaner.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CFGFILE="/build/diego/builder/sw/config"
+source $CFGFILE
+
+[ -n "$*" ]  && AGE=$(date --date="$*" +%s 2> /dev/null)
+[ -z "$AGE" ] && AGE=$(date --date="3 months ago" +%s)
+LASTCLEAN=$(cat $WEB/last_tag_clean 2> /dev/null || echo 0)
+
+LAST=$(egrep -h -o "builder_20.*\>" $WEB/*comp $WEB/*comp.pre|sort -u)
+HIST=$(egrep -h -o "builder_20.*\>" $WEB/*history | sort -u | \
+       egrep -v "${LAST// /|}")
+for TAG in $HIST; do
+    DATE=${TAG#*_}; DATE=${DATE%%_*}
+    TIME=${TAG%_*}; TIME=${TIME##*_}
+    TAGAGE=$(date --date="$DATE ${TIME//-/:}" +%s)
+    if [ $TAGAGE -lt $AGE -a $TAGAGE -ge $LASTCLEAN ]; then
+	cvs -Q rtag -d $TAG CMSDIST &> /dev/null || exit 1
+    fi
+done
+
+echo $AGE > $WEB/last_tag_clean
