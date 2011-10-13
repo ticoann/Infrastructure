524530bb45ba21bbf0d51cebe9dfdc070cb844f5
diego <diego@4525493e-7705-40b1-a816-d608a930855b>
2011-10-13 15:10:11 +0000

    Use correct parameters in stat for mac osx.
    From: Diego da Silva Gomes <diego@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Infrastructure/trunk@14255 4525493e-7705-40b1-a816-d608a930855b

--- BuilderAgent/log-cleaner.sh
+++ BuilderAgent/log-cleaner.sh
@@ -2,12 +2,13 @@
 
 CFGFILE="/build/dmwmbld/sw/current/config"
 source $CFGFILE
+source $BUILDFUNCS
 LOG_FILES="$BUILDER_ROOT/logs/*-*-*_*-*-*"
 
 [ -n "$*" ]  && AGE=$(date --date="$*" +%s 2> /dev/null)
 [ -z "$AGE" ] && AGE=$(date --date="1 week ago" +%s)
 for FILE in $LOG_FILES; do
-    LAST_CHANGE=$(stat -c %Z $FILE 2> /dev/null || date +%s)
+    LAST_CHANGE=$(stat $statfmt $FILE 2> /dev/null || date +%s)
     [ $LAST_CHANGE -lt $AGE ] && rm "$FILE"  &> /dev/null
 done
 
